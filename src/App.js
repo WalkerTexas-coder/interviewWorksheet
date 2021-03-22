d4c57fdc5df2f05c65e1a4f9d2ac66c6f4f432f7
Austin Walker <austinsrwalker@gmail.com>
2021-03-21 20:03:26 -0500

    Buy property

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import FetchRandomUser from './components/FetchRandomUser'
+import { useState } from 'react'
 
-function App() {
-  
+
+const App = () => {
+  const [count, setCount] = useState(0)
+ 
+ 
+ 
   return (
     <div className="App">
+      <div>counter: {count}</div>
+      <button onClick={()=> setCount(count + 1)}>increment</button>
       <FetchRandomUser/>
     </div>
   );
--- src/components/FetchRandomUser.js
+++ src/components/FetchRandomUser.js
@@ -3,6 +3,20 @@ import React, { useState, useEffect } from 'react'
 const FetchRandomUser = () => {
   
     const [person, setPerson] = useState(null);
+    const [num1, setNum1] = useState(0);
+    const [num2, setNum2] = useState(0);
+    const [multiplier, setMultiplier] = useState(0);
+
+
+     const addMultiply = (num1, num2, multiplier, cb) => {
+        const addedSum = num1 + num2 
+       console.log( multiply(multiplier, addedSum));
+        return
+    }
+
+    const multiply = (multiplier, givenSum) =>{
+        return multiplier * givenSum
+    }
   
     useEffect(async () => {
     const response = await fetch('https://api.randomuser.me/');
@@ -13,7 +27,20 @@ const FetchRandomUser = () => {
   
     return (
         <div>
-            {person && <h1> {JSON.stringify(person)} </h1>   }
+
+            {person && <h1> {`${person.name.title} ${person.name.first} ${person.name.last}` } </h1>   }
+            <form >
+                <label>
+                    <input type="number" onChange={(e)=> {setNum1(e.target.value)}}/>
+                </label>
+                <label>
+                    <input type="number" onChange={(e)=> {setNum2(e.target.value)}}/>
+                </label>
+                <label>
+                    <input type="number" onChange={(e)=> {setMultiplier(e.target.value)}}/>
+                </label>
+            </form>
+            <button onClick={()=>{addMultiply(num1, num2, multiplier, multiply)}}>FIRE!</button>
         </div>
     )
 }
